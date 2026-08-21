087bc3b70831a2b2a93cd22816ce8ec72a548390
Triumph Smart <triumphsmart.tech@gmail.com>
2026-08-21 10:58:03 +0100

    update API url

--- client/js/api.js
+++ client/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://gofuel-station-finder.onrender.com/api";
 
 // Helper function for API calls
 const apiRequest = async (
